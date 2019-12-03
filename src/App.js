c2a71399f4dde0962630db71facc3314bf8d27e5
Nicolás <nicolas.lobruno@wolox.com.ar>
2019-12-03 01:06:54 -0300

    Create componente Graphic and use in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+
+import Graphic from './components/Graphic';
+import { GRAPHICS } from './constants';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {GRAPHICS.map(graphic => (
+          <Fragment key={graphic.id}>
+            <Graphic
+              type={graphic.type}
+              width={graphic.width}
+              height={graphic.height}
+              data={graphic.data}
+            />
+            <hr className="separator" />
+          </Fragment>
+        ))}
       </header>
     </div>
   );
---
+++ src/components/Graphic/constants.js
@@ -0,0 +1,126 @@
+import { Radar, HorizontalBar, Line, Bar } from "react-chartjs-2";
+
+import {
+  formatNumberDecimal,
+  formatDateMoment,
+  formatPorcentage
+} from "./utils";
+
+export const graphicRadar = "radar";
+export const graphicBar = "bar";
+export const graphicHorizontalBar = "horizontalBar";
+export const graphicLine = "line";
+
+export const typesGraphic = {
+  [graphicRadar]: Radar,
+  [graphicBar]: Bar,
+  [graphicHorizontalBar]: HorizontalBar,
+  [graphicLine]: Line
+};
+
+const GRAPHIC_IDS = {
+  BAR: 1,
+  LINE: 2
+};
+
+export const tooltipsGraphic = (graphicId, currency) => {
+  switch (graphicId) {
+    case GRAPHIC_IDS.BAR: {
+      return {
+        title: tooltipItem => tooltipItem[0].yLabel,
+        label: (tooltipItem, data) => `Ventas ${tooltipItem.xLabel}`
+      };
+    }
+    case GRAPHIC_IDS.LINE: {
+      return {
+        title: (tooltipItem, data) => {
+          const date = data.labels[tooltipItem[0].index].split("|");
+          return `Date ${
+            tooltipItem.length > 1
+              ? data.labels[tooltipItem[0].index]
+              : date[tooltipItem[0].datasetIndex]
+          }`;
+        },
+        label: (tooltipItem, data) =>
+          `${
+            data.datasets[tooltipItem.datasetIndex].label
+          }: ${currency} ${formatNumberDecimal(tooltipItem.yLabel)}`,
+        afterLabel: (tooltipItem, data) => {
+          const dataSetsValues = data.datasets.map(dataSet =>
+            parseInt(dataSet.data[tooltipItem.index], 10)
+          );
+          const percent =
+            (dataSetsValues.reduce((acum = 0, value) => acum / value) - 1) *
+            100;
+          return tooltipItem.datasetIndex === data.datasets.length - 1 &&
+            !isNaN(percent) &&
+            percent !== Infinity
+            ? `Variación: ${formatPorcentage(percent.toFixed(1))}%`
+            : null;
+        }
+      };
+    }
+    default: {
+      return [];
+    }
+  }
+};
+
+export const graphicRadarOptions = graphicId => {
+  const optionGraphic = {
+    responsive: true,
+    tooltips: {
+      mode: "x",
+      intersect: false,
+      callbacks: tooltipsGraphic(graphicId)
+    },
+    hover: {
+      mode: "x",
+      intersect: false
+    },
+    legend: { display: false },
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            callback: value => {
+              const date = value.split(" | ");
+              return formatDateMoment(date[0], "DD-MM-YYYY", "DD-MM-YY");
+            }
+          }
+        }
+      ]
+    }
+  };
+  return optionGraphic;
+};
+
+export const graphicOptionsLine = graphicId => {
+  const optionGraphic = {
+    responsive: true,
+    tooltips: {
+      mode: "x",
+      intersect: false,
+      callbacks: tooltipsGraphic(graphicId)
+    },
+    hover: {
+      mode: "x",
+      intersect: false
+    }
+  };
+  return optionGraphic;
+};
+
+export function optionsForGraphic(type, graphicId, currency) {
+  switch (type) {
+    case graphicRadar: {
+      return graphicRadarOptions(graphicId, currency);
+    }
+    case graphicLine: {
+      return graphicOptionsLine(graphicId, currency);
+    }
+    default: {
+      return {};
+    }
+  }
+}
